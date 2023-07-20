1d6949bd8c8ccdba002a03b77b61ee2ce153dce4
Daniel Jolo <danieljolo4899@gmail.com>
2023-07-20 16:20:01 +0800

    add api route

--- api/api_routes.tsx
+++ api/api_routes.tsx
@@ -1,8 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import api from "./api_server";
 
+const headerAuth = (token?: string | null) => {
+  return {
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  };
+};
+
 export const routesPostApi = async (routeName: string, params: any) => {
+  const token = await AsyncStorage.getItem("token");
   return api
-    .post(routeName, params)
+    .post(routeName, params, headerAuth(token))
     .then((res) => {
       return res;
     })
@@ -12,5 +23,21 @@ export const routesPostApi = async (routeName: string, params: any) => {
         data: {},
         status,
       };
+    });
+};
+
+export const routesGetApi = async (routeName: string) => {
+  const token = await AsyncStorage.getItem("token");
+  return api
+    .get(routeName, headerAuth(token))
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      const response = {
+        data: {},
+        status: err.response.status,
+      };
+      return response;
     });
 };
--- screens/Logged/Home.tsx
+++ screens/Logged/Home.tsx
@@ -5,67 +5,198 @@ import {
   Dimensions,
   Image,
   TextInput,
+  ActivityIndicator,
+  FlatList,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { shallow } from "zustand/shallow";
 import { tokenStore } from "../../zustand/logintoken";
+import { useFocusEffect } from "@react-navigation/native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 import Ionic from "react-native-vector-icons/Ionicons";
+import { routesGetApi } from "../../api/api_routes";
 
 const { height, width } = Dimensions.get("window");
 
+interface FriendsInterface {
+  email?: string;
+  name?: string;
+  _id?: string;
+}
+
 const Home = () => {
   // zustand
-  const { storeTokenAction } = tokenStore((state) => state, shallow);
+  const { clearToken } = tokenStore((state) => state, shallow);
 
+  const [loading, setLoading] = useState<boolean>(true);
   const [showSearch, setShowSearch] = useState<boolean>(false);
+  const [friends, setFriends] = useState<FriendsInterface[]>([]);
+  const [allUsers, setAllUsers] = useState<FriendsInterface[]>([]);
+  const [displayType, setDisplayType] = useState<string>("");
+
+  useFocusEffect(
+    useCallback(() => {
+      getUserFriends();
+      getAllUsers();
+    }, [])
+  );
 
   const logout = async (): Promise<void> => {
     await AsyncStorage.removeItem("token");
-    storeTokenAction("");
+    clearToken();
   };
 
-  const friends = [
-    {
-      id: 1,
-      image: require("../../assets/testimage2.png"),
-      name: "Dolce Amore",
-      desc: "Lorem Ipsum is lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-    {
-      id: 2,
-      image: require("../../assets/testimage2.png"),
-      name: "Dolce Amore",
-      desc: "Lorem Ipsum is lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-    {
-      id: 3,
-      image: require("../../assets/testimage2.png"),
-      name: "Dolce Amore",
-      desc: "Lorem Ipsum is lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-    {
-      id: 4,
-      image: require("../../assets/testimage2.png"),
-      name: "Dolce Amore",
-      desc: "Lorem Ipsum is lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-    {
-      id: 5,
-      image: require("../../assets/testimage2.png"),
-      name: "Dolce Amore",
-      desc: "Lorem Ipsum is lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-    {
-      id: 6,
-      image: require("../../assets/testimage2.png"),
-      name: "Dolce Amore",
-      desc: "Lorem Ipsum is lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    },
-  ];
-  
+  const getUserFriends = async (): Promise<void> => {
+    setLoading(true);
+    await routesGetApi("/user")
+      .then((response) => {
+        if (response.status === 200) {
+          setFriends(response.data.data);
+        }
+      })
+      .finally(() => setLoading(false));
+  };
+
+  const getAllUsers = async (): Promise<void> => {
+    await routesGetApi("/user").then((response) => {
+      if (response.status === 200) {
+        setAllUsers(response.data.data);
+      }
+    });
+  };
+
+  const renderFriendList = () => {
+    return (
+      displayType !== "search" && (
+        <View style={{ flex: 1, paddingHorizontal: width * 0.05 }}>
+          {friends.map((item) => (
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginVertical: height * 0.01,
+              }}
+              key={item?._id}
+            >
+              <Image
+                style={{
+                  height: height * 0.075,
+                  width: width * 0.15,
+                  borderRadius: 100,
+                }}
+                source={require("../../assets/testimage2.png")}
+              />
+              <View
+                style={{
+                  paddingLeft: width * 0.02,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <View style={{ flexDirection: "column" }}>
+                  <Text
+                    style={{
+                      fontFamily: "Poppins500",
+                      fontSize: height * 0.019,
+                      color: "#050505",
+                    }}
+                  >
+                    {item?.name}
+                  </Text>
+                  <Text
+                    numberOfLines={1}
+                    ellipsizeMode="tail"
+                    style={{
+                      width: width * 0.62,
+                      fontFamily: "Poppins",
+                      fontSize: height * 0.015,
+                      color: "#050505",
+                    }}
+                  >
+                    {/* {desc} */}
+                  </Text>
+                </View>
+                <View style={{ flexDirection: "column", alignItems: "center" }}>
+                  <Text>3:05 PM</Text>
+                  <Text
+                    style={{
+                      fontFamily: "Poppins",
+                      fontSize: height * 0.0125,
+                      borderRadius: 100,
+                      backgroundColor: "#050505",
+                      paddingHorizontal: width * 0.02,
+                      paddingVertical: height * 0.001,
+                      color: "white",
+                      textAlign: "center",
+                    }}
+                  >
+                    1
+                  </Text>
+                </View>
+              </View>
+            </View>
+          ))}
+        </View>
+      )
+    );
+  };
+
+  const renderSearchList = () => {
+    return (
+      displayType === "search" && (
+        <View
+          style={{
+            paddingHorizontal: wp("5%"),
+            paddingVertical: hp("1%"),
+          }}
+        >
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={allUsers}
+            renderItem={({ item }) => {
+              return (
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    paddingBottom: hp("1%"),
+                  }}
+                  key={item?._id}
+                >
+                  <Image
+                    style={styles.userimage}
+                    source={require("../../assets/testimage2.png")}
+                  />
+                  <Text style={styles.textname}>{item?.name}</Text>
+                </View>
+              );
+            }}
+          />
+        </View>
+      )
+    );
+  };
+
+  const display = () => {
+    return !loading ? (
+      <View style={{ flex: 1 }}>
+        {renderFriendList()}
+        {renderSearchList()}
+      </View>
+    ) : (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View
@@ -93,7 +224,10 @@ const Home = () => {
               color="#050505"
             />
             <Ionic
-              onPress={() => setShowSearch(!showSearch)}
+              onPress={() => {
+                setDisplayType("search");
+                setShowSearch(!showSearch);
+              }}
               size={height * 0.03}
               name="search"
               color="#050505"
@@ -119,7 +253,10 @@ const Home = () => {
               autoFocus={true}
             />
             <Ionic
-              onPress={() => setShowSearch(false)}
+              onPress={() => {
+                setDisplayType("");
+                setShowSearch(false);
+              }}
               style={styles.iconDesign}
               size={height * 0.025}
               name="arrow-back-outline"
@@ -128,76 +265,7 @@ const Home = () => {
         </View>
       )}
 
-      <View style={{ flex: 1, paddingHorizontal: width * 0.05 }}>
-        {friends.map((item) => (
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              marginVertical: height * 0.01,
-            }}
-            key={item.id}
-          >
-            <Image
-              style={{
-                height: height * 0.075,
-                width: width * 0.15,
-                borderRadius: 100,
-              }}
-              source={item.image}
-            />
-            <View
-              style={{
-                paddingLeft: width * 0.02,
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-              }}
-            >
-              <View style={{ flexDirection: "column" }}>
-                <Text
-                  style={{
-                    fontFamily: "Poppins500",
-                    fontSize: height * 0.019,
-                    color: "#050505",
-                  }}
-                >
-                  {item.name}
-                </Text>
-                <Text
-                  numberOfLines={1}
-                  ellipsizeMode="tail"
-                  style={{
-                    width: width * 0.62,
-                    fontFamily: "Poppins",
-                    fontSize: height * 0.015,
-                    color: "#050505",
-                  }}
-                >
-                  {item.desc}
-                </Text>
-              </View>
-              <View style={{ flexDirection: "column", alignItems: "center" }}>
-                <Text>3:05 PM</Text>
-                <Text
-                  style={{
-                    fontFamily: "Poppins",
-                    fontSize: height * 0.0125,
-                    borderRadius: 100,
-                    backgroundColor: "#050505",
-                    paddingHorizontal: width * 0.02,
-                    paddingVertical: height * 0.001,
-                    color: "white",
-                    textAlign: "center",
-                  }}
-                >
-                  1
-                </Text>
-              </View>
-            </View>
-          </View>
-        ))}
-      </View>
+      {display()}
     </SafeAreaView>
   );
 };
@@ -205,7 +273,6 @@ const Home = () => {
 const styles = StyleSheet.create({
   serachinput: {
     width: width,
-    // borderWidth: 1,
     height: height * 0.07,
     paddingVertical: height * 0.015,
     paddingLeft: width * 0.15,
@@ -221,6 +288,18 @@ const styles = StyleSheet.create({
     left: width * 0.05,
     top: height * 0.035,
     elevation: 2,
+  },
+  textname: {
+    fontFamily: "Poppins500",
+    fontSize: hp("1.8%"),
+    lineHeight: hp("2.7%"),
+    color: "#050505",
+    paddingLeft: wp("2.5%"),
+  },
+  userimage: {
+    height: hp("5%"),
+    width: wp("10%"),
+    borderRadius: 100,
   },
 });
 
